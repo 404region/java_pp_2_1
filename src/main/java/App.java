d463528595a3c1cb37e8bdb9f5e14ab50e247035
Ann S <404region@gmail.com>
2023-10-13 12:34:01 +0300

    pp_2_1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,24 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+
+
+        Cat cat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+
+
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getMessage());
+
+        System.out.println("`beans Hello: " + (bean == bean2));
+
+        System.out.println("`beans Cats: " + (cat == cat2));
     }
 }
